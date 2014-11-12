f9c60e97e98b36b0d634f7ab9afb207d17e2ebe9
Jim Fathman <jfathman@esi-estech.com>
2014-11-12 17:14:34 +0000

    Remove docker push.

--- docker.sh
+++ docker.sh
@@ -37,12 +37,6 @@ main()
       bash)
         sudo docker run --name app-$$ --rm -i -t -p ${PORT}:${PORT} ${APP_NAME}:${APP_VERSION} /bin/bash
         ;;
-      push)
-        getArtifactoryAccount
-        sudo docker tag ${APP_NAME}:${APP_VERSION} ${ARTIFACTORY_ACCOUNT}.artifactoryonline.com/${APP_NAME}:${APP_VERSION}
-        sudo docker push ${ARTIFACTORY_ACCOUNT}.artifactoryonline.com/${APP_NAME}:${APP_VERSION}
-        sudo docker rmi ${ARTIFACTORY_ACCOUNT}.artifactoryonline.com/${APP_NAME}:${APP_VERSION}
-        ;;
       help | -help | --help)
         usage 0
         ;;
@@ -60,21 +54,8 @@ usage()
   echo "$0 purge     Remove untagged images after Docker reuses repo:tag for new build"
   echo "$0 retrieve  Retrieve build artifacts from Docker container"
   echo "$0 bash      Run bash in Docker container"
-  echo "$0 push      Push Docker image to Artifactory repository"
   echo "$0 help      Display help information"
   exit "$1"
-}
-
-getArtifactoryAccount()
-{
-  # Precondition: ~/.dockercfg contains a single subdomain for artifactory.
-  # Parse ~/.dockercfg, select subdomain from subdomain.artifactory.com, use it as artifactory account name.
-  SUBDOMAIN=$(sudo cat ~/.dockercfg | jq -r 'keys | .[]' | grep artifactory | awk -F/ '{print $3}' | cut -f1 -d.)
-  if [ -z "$SUBDOMAIN" ]; then
-    echo "ERROR: could not parse artifactory subdomain from ~/.dockercfg"
-    exit 1
-  fi
-  ARTIFACTORY_ACCOUNT=${SUBDOMAIN}
 }
 
 main "$@"
